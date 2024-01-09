272f7676b2a6fa3bd2584c3cc869899610ebcaf9
MeathSpace <40881591+MeathSpace@users.noreply.github.com>
2024-01-09 14:57:02 +0530

    Update adminRoutes.js
    Sample to get Sonar Results

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -3,6 +3,7 @@ const { registerAdmin, loginAdmin, adminChange, getAdmin } = require("../control
 const router = express.Router()
 
 router.route('/signup').post(registerAdmin)
+
 router.route("/signin").post(loginAdmin)
 router.route("/:id/:Refid/:requestedName").put(adminChange)
 router.route("/getadmin").get(getAdmin)
